143c25aecc564e72bc0def8f714fb97c855f0d0f
Brian <blsvsanteliz@gmail.com>
2021-01-29 00:45:30 -0400

    setup basic apollo configured

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
 
-Vue.config.productionTip = false
+const httpLink = createHttpLink({
+  uri: "https://graphqlzero.almansi.me/api",
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+Vue.config.productionTip = false;
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+Vue.use(VueApollo);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  apolloProvider,
+}).$mount("#app");
