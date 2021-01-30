68c48dd419b36fcff5c10f09a6a2abeca0e1f7e5
Brian <blsvsanteliz@gmail.com>
2021-01-29 20:16:12 -0400

    add ApolloQuery Component, added

---
+++ src/components/Albums.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h3>{{ id }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Albums",
+  props: {
+    title: {
+      type: String,
+    },
+    id: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import VueApollo from "vue-apollo";
-
-const httpLink = createHttpLink({
-  uri: "https://graphqlzero.almansi.me/api",
-});
-
-const cache = new InMemoryCache();
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-});
-Vue.config.productionTip = false;
-const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-});
-Vue.use(VueApollo);
-
+import { createProvider } from "./vue-apollo";
 new Vue({
   render: (h) => h(App),
-  apolloProvider,
+  apolloProvider: createProvider(),
 }).$mount("#app");
