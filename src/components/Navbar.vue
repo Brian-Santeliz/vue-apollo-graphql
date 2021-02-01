3f9d566107fa44411193912d51ce87f3d704b214
Brian <blsvsanteliz@gmail.com>
2021-02-01 09:04:12 -0400

    add navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    <router-link to="/add">Form</router-link> <br />
-    <router-link to="/">HOme</router-link>
-  </div>
+  <mdb-navbar expand="large" dark color="indigo">
+    <router-link to="/">
+      <mdb-navbar-brand href="#"> Vuejs & Apollo </mdb-navbar-brand>
+    </router-link>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <router-link to="/add">
+          <mdb-nav-item active> Form </mdb-nav-item>
+        </router-link>
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
 </template>
 
 <script>
+import {
+  mdbNavbar,
+  mdbNavbarBrand,
+  mdbNavbarToggler,
+  mdbNavbarNav,
+  mdbNavItem,
+} from "mdbvue";
+
 export default {
   name: "Navbar",
+  components: {
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavbarNav,
+    mdbNavItem,
+  },
 };
 </script>
 
